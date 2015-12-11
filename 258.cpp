6d79c1adec014dea18606f498864b056e927f661
Gabriel Berlanga <g.berlanga.s@gmail.com>
2015-12-11 15:08:21 -0600

    problem 258 done

--- 258.cpp
+++ 258.cpp
@@ -1,1 +1,26 @@
-258
+/*Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+For example:
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Solution
+mod 10 te da el ultimo digito del entero. aun que no es la forma mas eficiente con un while
+*/
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+    	int sum = 0;
+        while (num > 0){
+        	sum += (num % 10);
+        	num = num/10;
+        }
+
+        if(sum >= 10){
+        	addDigits(sum);
+        }
+        else{
+        	return sum;
+        }
+    }
+};
